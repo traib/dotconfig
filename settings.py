6290d50a98a39caecb241283e3e94e72b648f800
traib <traib@users.noreply.github.com>
2022-02-08 00:00:00 -0800

    Split repository into a separate module

--- settings.py
+++ settings.py
@@ -1,373 +1,217 @@
-import dataclasses
-import enum
+import sys
+
+sys.dont_write_bytecode = True
+assert sys.version_info >= (3, 9)
+assert __name__ == '__main__'
+
+import argparse
+import contextlib
+import difflib
+import graphlib
+import io
 import os
 import pathlib
-import platform
 import shutil
-import sys
-from typing import NamedTuple, Optional
+import subprocess
+import tempfile
 
-assert sys.version_info >= (3, 9)
-
-SCRIPT_DIR = pathlib.Path(__file__).resolve().parent
-REPOSITORY = SCRIPT_DIR.joinpath('repository')
+from repository import REPOSITORY, Category
 
 
-@dataclasses.dataclass(frozen=True)
-class Location:
-    repo: str
-    linux: Optional[str]
-    darwin: Optional[str]
-    windows: Optional[str]
-
-    def __init__(self, repo, linux=None, darwin=None, windows=None):
-        assert repo
-        object.__setattr__(self, 'repo', repo)
-        object.__setattr__(self, 'linux', linux)
-        object.__setattr__(self, 'darwin', darwin)
-        object.__setattr__(self, 'windows', windows)
-
-    def inside_repository(self) -> pathlib.Path:
-        return REPOSITORY.joinpath(self.repo)
-
-    def outside_repository(self) -> Optional[pathlib.Path]:
-        load = getattr(self, platform.system().lower())
-        return None if not load else pathlib.Path(os.path.expandvars(load))
+def as_categories(
+    names: tuple[str], default=tuple(Category)
+) -> tuple[Category]:
+    return default if not names else tuple(
+        Category[name.upper()] for name in names
+    )
 
 
-@dataclasses.dataclass(frozen=True)
-class Command:
-    args: tuple[str]
-
-    def __init__(self, *args):
-        assert args
-        object.__setattr__(self, 'args', args)
-
-    def on_current_platform(self) -> tuple[str]:
-        return (shutil.which(self.args[0]),) + self.args[1:]
-
-
-class CategoryDescription(NamedTuple):
-    prerequisites: tuple[str] = ()
-    before_install: tuple[Command] = ()
-    locations: tuple[Location] = ()
-    after_install: tuple[Command] = ()
-
-    def is_disabled(self) -> bool:
-        return all(
-            not location.outside_repository() for location in self.locations
+def topological_sort(names: tuple[str]) -> tuple[Category]:
+    visited = set()
+    to_visit = list(as_categories(names))
+    sorter = graphlib.TopologicalSorter()
+    while to_visit:
+        category = to_visit.pop()
+        prerequisites = as_categories(category.prerequisites, default=())
+        visited.add(category)
+        sorter.add(category, *prerequisites)
+        to_visit.extend(
+            prerequisite for prerequisite in prerequisites
+            if prerequisite not in visited
         )
+    return sorter.static_order()
 
 
-class Category(CategoryDescription, enum.Enum):
-
-    BASH = CategoryDescription(
-        prerequisites=('SH',),
-        locations=(
-            Location(
-                repo='bash/bash_profile',
-                linux='$HOME/.bash_profile',
-                darwin='$HOME/.bash_profile',
-                windows='$HOME/.bash_profile'
-            ),
-            Location(
-                repo='bash/bashrc',
-                linux='$HOME/.bashrc',
-                darwin='$HOME/.bashrc',
-                windows='$HOME/.bashrc'
-            ),
-        ),
-    )
-
-    GIT = CategoryDescription(
-        locations=(
-            Location(
-                repo='git/config',
-                linux='$HOME/.gitconfig',
-                darwin='$HOME/.gitconfig',
-                windows='$HOME/.gitconfig'
-            ),
-        ),
-    )
-
-    JETBRAINS = CategoryDescription(
-        locations=(
-            # https://www.jetbrains.com/help/idea/directories-used-by-the-ide-to-store-settings-caches-plugins-and-logs.html#config-directory
-            Location(
-                repo='jetbrains/',
-                linux='$HOME/.config/JetBrains/',
-                darwin='$HOME/Library/Application Support/JetBrains/',
-                windows='%APPDATA%/JetBrains/'
-            ),
-        ),
-    )
-
-    SH = CategoryDescription(
-        locations=(
-            Location(
-                repo='sh/inputrc',
-                linux='$HOME/.inputrc',
-                darwin='$HOME/.inputrc',
-                windows='$HOME/.inputrc'
-            ),
-            Location(
-                repo='sh/profile',
-                linux='$HOME/.profile',
-                darwin='$HOME/.profile',
-                windows='$HOME/.profile'
-            ),
-        ),
-    )
-
-    VSCODE = CategoryDescription(
-        locations=(
-            # https://code.visualstudio.com/docs/getstarted/settings#_settings-file-locations
-            Location(
-                repo='vscode/',
-                linux='$HOME/.config/Code/',
-                darwin='$HOME/Library/Application Support/Code/',
-                windows='%APPDATA%/Code/'
-            ),
-        ),
-        after_install=(
-            Command(
-                'code',
-                '--install-extension',
-                'vscodevim.vim',
-            ),
-        ),
-    )
-
-    ZSH = CategoryDescription(
-        prerequisites=('SH',),
-        before_install=(
-            Command(
-                'curl', '--silent', '--show-error',
-                'https://raw.githubusercontent.com/grml/grml-etc-core/master/etc/zsh/zshrc',
-                '--output', REPOSITORY.joinpath('zsh/zshrc')
-            ),
-        ),
-        locations=(
-            # https://wiki.archlinux.org/index.php/zsh#Startup/Shutdown_files
-            Location(
-                repo='zsh/zshrc.pre',
-                linux='$HOME/.zshrc.pre',
-                darwin='$HOME/.zshrc.pre'
-            ),
-            Location(
-                repo='zsh/zshrc', linux='$HOME/.zshrc', darwin='$HOME/.zshrc'
-            ),
-            Location(
-                repo='zsh/zshrc.local',
-                linux='$HOME/.zshrc.local',
-                darwin='$HOME/.zshrc.local'
-            ),
-        ),
-    )
+def open_or_empty(path: pathlib.Path):
+    if not path.is_file():
+        return contextlib.nullcontext(io.StringIO())
+    return open(path)
 
 
-if __name__ == '__main__':
-    import argparse
-    import contextlib
-    import difflib
-    import graphlib
-    import io
-    import shutil
-    import subprocess
-    import tempfile
+def mkparents(path: pathlib.Path):
+    for parent in reversed(path.parents):
+        if not parent.exists():
+            parent.mkdir(mode=0o700, parents=False, exist_ok=True)
 
-    def as_categories(
-        names: tuple[str], default=tuple(Category)
-    ) -> tuple[Category]:
-        return default if not names else tuple(
-            Category[name.upper()] for name in names
-        )
 
-    def topological_sort(names: tuple[str]) -> tuple[Category]:
-        visited = set()
-        to_visit = list(as_categories(names))
-        sorter = graphlib.TopologicalSorter()
-        while to_visit:
-            category = to_visit.pop()
-            prerequisites = as_categories(category.prerequisites, default=())
-            visited.add(category)
-            sorter.add(category, *prerequisites)
-            to_visit.extend(
-                prerequisite for prerequisite in prerequisites
-                if prerequisite not in visited
-            )
-        return sorter.static_order()
+def symlink_force(src: pathlib.Path, dst: pathlib.Path):
+    with tempfile.TemporaryDirectory(dir=REPOSITORY.joinpath('tmp')) as tmp_dir:
+        tmp_symlink = pathlib.Path(tmp_dir).joinpath('symlink')
+        tmp_symlink.symlink_to(src)
+        tmp_symlink.replace(dst)
 
-    def open_or_empty(path: pathlib.Path):
-        if not path.is_file():
-            return contextlib.nullcontext(io.StringIO())
-        return open(path)
 
-    def mkparents(path: pathlib.Path):
-        for parent in reversed(path.parents):
-            if not parent.exists():
-                parent.mkdir(mode=0o700, parents=False, exist_ok=True)
+def cp_force(src: pathlib.Path, dst: pathlib.Path):
+    with tempfile.TemporaryDirectory(dir=REPOSITORY.joinpath('tmp')) as tmp_dir:
+        tmp_cp = pathlib.Path(tmp_dir).joinpath('cp')
+        shutil.copyfile(src, tmp_cp, follow_symlinks=False)
+        tmp_cp.replace(dst)
 
-    def symlink_force(src: pathlib.Path, dst: pathlib.Path):
-        with tempfile.TemporaryDirectory(
-            dir=REPOSITORY.joinpath('tmp')
-        ) as tmp_dir:
-            tmp_symlink = pathlib.Path(tmp_dir).joinpath('symlink')
-            tmp_symlink.symlink_to(src)
-            tmp_symlink.replace(dst)
 
-    def cp_force(src: pathlib.Path, dst: pathlib.Path):
-        with tempfile.TemporaryDirectory(
-            dir=REPOSITORY.joinpath('tmp')
-        ) as tmp_dir:
-            tmp_cp = pathlib.Path(tmp_dir).joinpath('cp')
-            shutil.copyfile(src, tmp_cp, follow_symlinks=False)
-            tmp_cp.replace(dst)
+def install(args):
+    for category in topological_sort(args.categories):
+        if category.is_disabled():
+            continue
+        print()
+        print(category)
+        print('=' * len(str(category)))
 
-    def install(args):
-        for category in topological_sort(args.categories):
-            if category.is_disabled():
-                continue
+        for command in category.before_install:
+            command = command.on_current_platform()
             print()
-            print(category)
-            print('=' * len(str(category)))
+            print(f'run{command}')
 
-            for command in category.before_install:
-                command = command.on_current_platform()
-                print()
-                print(f'run{command}')
+            if args.dry_run:
+                continue
 
-                if args.dry_run:
-                    continue
-
-                print(
-                    subprocess.check_output(
-                        command, stderr=subprocess.STDOUT, text=True
-                    )
+            print(
+                subprocess.check_output(
+                    command, stderr=subprocess.STDOUT, text=True
                 )
+            )
 
-            for location in category.locations:
-                operation = symlink_force if not args.cp else cp_force
-                operation_name = 'symlink' if not args.cp else 'cp'
+        for location in category.locations:
+            operation = symlink_force if not args.cp else cp_force
+            operation_name = 'symlink' if not args.cp else 'cp'
 
-                src = location.inside_repository()
-                dst = location.outside_repository()
-                if not dst:
-                    continue
+            src = location.inside_repository()
+            dst = location.outside_repository()
+            if not dst:
+                continue
 
-                operation_paths = []
+            operation_paths = []
 
-                if src.is_file():
-                    operation_paths.append((src, dst))
+            if src.is_file():
+                operation_paths.append((src, dst))
 
-                if src.is_dir():
-                    for dirpath, _, filenames in os.walk(src):
-                        if not filenames:
-                            continue
-
-                        for filename in filenames:
-                            dir = pathlib.PurePath(dirpath)
-                            rel = dir.relative_to(src).joinpath(filename)
-                            operation_paths.append(
-                                (src.joinpath(rel), dst.joinpath(rel))
-                            )
-
-                for src_path, dst_path in operation_paths:
-                    print()
-                    print(
-                        f"{operation_name}(src='{src_path}', dst='{dst_path}')"
-                    )
-
-                    if args.dry_run:
+            if src.is_dir():
+                for dirpath, _, filenames in os.walk(src):
+                    if not filenames:
                         continue
 
-                    mkparents(dst_path)
-                    operation(src_path, dst_path)
+                    for filename in filenames:
+                        dir = pathlib.PurePath(dirpath)
+                        rel = dir.relative_to(src).joinpath(filename)
+                        operation_paths.append(
+                            (src.joinpath(rel), dst.joinpath(rel))
+                        )
 
-            for command in category.after_install:
-                command = command.on_current_platform()
+            for src_path, dst_path in operation_paths:
                 print()
-                print(f'run{command}')
+                print(f"{operation_name}(src='{src_path}', dst='{dst_path}')")
 
                 if args.dry_run:
                     continue
 
-                print(
-                    subprocess.check_output(
-                        command, stderr=subprocess.STDOUT, text=True
-                    )
-                )
+                mkparents(dst_path)
+                operation(src_path, dst_path)
 
-    def diff(args):
-        for category in topological_sort(args.categories):
-            if category.is_disabled():
-                continue
+        for command in category.after_install:
+            command = command.on_current_platform()
             print()
-            print(category)
-            print('=' * len(str(category)))
+            print(f'run{command}')
 
-            for location in category.locations:
-                src = location.inside_repository()
-                dst = location.outside_repository()
-                if not dst:
-                    continue
+            if args.dry_run:
+                continue
 
-                diff_paths = []
+            print(
+                subprocess.check_output(
+                    command, stderr=subprocess.STDOUT, text=True
+                )
+            )
 
-                if src.is_file():
-                    diff_paths.append((src, dst))
 
-                if src.is_dir():
-                    for dirpath, _, filenames in os.walk(src):
-                        if not filenames:
-                            continue
+def diff(args):
+    for category in topological_sort(args.categories):
+        if category.is_disabled():
+            continue
+        print()
+        print(category)
+        print('=' * len(str(category)))
 
-                        for filename in filenames:
-                            dir = pathlib.PurePath(dirpath)
-                            rel = dir.relative_to(src).joinpath(filename)
-                            diff_paths.append(
-                                (src.joinpath(rel), dst.joinpath(rel))
+        for location in category.locations:
+            src = location.inside_repository()
+            dst = location.outside_repository()
+            if not dst:
+                continue
+
+            diff_paths = []
+
+            if src.is_file():
+                diff_paths.append((src, dst))
+
+            if src.is_dir():
+                for dirpath, _, filenames in os.walk(src):
+                    if not filenames:
+                        continue
+
+                    for filename in filenames:
+                        dir = pathlib.PurePath(dirpath)
+                        rel = dir.relative_to(src).joinpath(filename)
+                        diff_paths.append(
+                            (src.joinpath(rel), dst.joinpath(rel))
+                        )
+
+            for src_path, dst_path in diff_paths:
+                with open_or_empty(src_path) as src_file:
+                    with open_or_empty(dst_path) as dst_file:
+                        deltas = list(
+                            difflib.unified_diff(
+                                src_file.readlines(),
+                                dst_file.readlines(),
+                                fromfile=str(src_path),
+                                tofile=str(dst_path),
+                                n=0
                             )
+                        )
+                        if deltas:
+                            print()
+                            print(''.join(deltas))
 
-                for src_path, dst_path in diff_paths:
-                    with open_or_empty(src_path) as src_file:
-                        with open_or_empty(dst_path) as dst_file:
-                            deltas = list(
-                                difflib.unified_diff(
-                                    src_file.readlines(),
-                                    dst_file.readlines(),
-                                    fromfile=str(src_path),
-                                    tofile=str(dst_path),
-                                    n=0
-                                )
-                            )
-                            if deltas:
-                                print()
-                                print(''.join(deltas))
 
-    parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(dest='subcommand', required=True)
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers(dest='subcommand', required=True)
 
-    installparser = subparsers.add_parser('install')
-    installparser.set_defaults(handler=install)
-    installparser.add_argument('--dry-run', action='store_true')
-    installparser.add_argument('--cp', action='store_true')
-    installparser.add_argument(
-        'categories',
-        nargs='*',
-        choices=[''] + [category.name.lower() for category in Category],
-        default=''
-    )
+installparser = subparsers.add_parser('install')
+installparser.set_defaults(handler=install)
+installparser.add_argument('--dry-run', action='store_true')
+installparser.add_argument('--cp', action='store_true')
+installparser.add_argument(
+    'categories',
+    nargs='*',
+    choices=[''] + [category.name.lower() for category in Category],
+    default=''
+)
 
-    diffparser = subparsers.add_parser('diff')
-    diffparser.set_defaults(handler=diff)
-    diffparser.add_argument(
-        'categories',
-        nargs='*',
-        choices=[''] + [category.name.lower() for category in Category],
-        default=''
-    )
+diffparser = subparsers.add_parser('diff')
+diffparser.set_defaults(handler=diff)
+diffparser.add_argument(
+    'categories',
+    nargs='*',
+    choices=[''] + [category.name.lower() for category in Category],
+    default=''
+)
 
-    args = parser.parse_args()
-    print(args)
-    args.handler(args)
+args = parser.parse_args()
+print(args)
+args.handler(args)
